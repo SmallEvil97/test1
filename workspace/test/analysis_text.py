af9461458b565df7b30711b4688d6392cc39cf37
SmallEvil97 <SmallEvil97@gmail.com>
2015-05-09 16:16:13 +0300

    Delete analysis_text.py

--- workspace/test/analysis_text.py
+++
@@ -1,8 +0,0 @@
-import string
-words = {} 
-for line in open('my_text'): 
-    for word in line.lower().split():  
-        word = word.strip(string.punctuation)
-        words[word] = words.get(word, 0) + 1  
-for word in sorted(words): 
-    print("'{0}'".format(word),"found:",words[word]) 
