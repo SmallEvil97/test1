34424d601e66d79194a028e675fe1181e9584a30
Bogdan Romanov <smallevil97@gmail.com>
2015-05-09 18:27:13 +0300

    correction

--- TestSimpicity.py
+++ TestSimpicity.py
@@ -1,4 +1,4 @@
-from Test1py3.function import test_simplicity
+from function import test_simplicity
 try:
     f = open('my_number', 'r')
     number = int(f.read())
--- chart2.py
+++ chart2.py
@@ -1,17 +1,24 @@
-from SandBox.analysis_text1 import *
+from analysis_text import *
 import pylab
 import time
 dat = []
 ylis = [25,12.5,8.75,7.5,5,5,3.75,3.75,2.6,2.6,2.6,2.6,2,2,2,2,2,2,2,2]
+dat.extend(b)
+e = 2
 pylab.ion()
-for n in range(30):
-    ylist = [(y + n / 2) for y in ylis]
-    dat.extend(b)
+for n in range(10):
+    q = [(e + n / 2)]
+    ylis.extend(q)
+    dat.extend(q)
     pylab.subplot (2, 1, 1)
     pylab.plot (dat,'go-')
     pylab.title ("Chart Analysis_Text")
     pylab.subplot (2, 1, 2)
-    pylab.plot (ylist,'go-')
+    pylab.plot (ylis,'ro-')
     pylab.title ("Chart")        
     pylab.draw()
+    if e == 2:
+        e += 6
+    else:
+        e -= 2       
     time.sleep(1)
