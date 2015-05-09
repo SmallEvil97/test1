544431d0987db0a5fb8f00aadaf6bfa0ce7d3875
SmallEvil97 <SmallEvil97@gmail.com>
2015-05-09 15:54:17 +0300

    Delete TestSimpicity.py

--- test/TestSimpicity.py
+++
@@ -1,8 +0,0 @@
-from Test1py3.function import test_simplicity
-try:
-    f = open('my_number', 'r')
-    number = int(f.read())
-except ValueError:
-    print("Enter the correct number!!!")
-else:
-    test_simplicity(number)
